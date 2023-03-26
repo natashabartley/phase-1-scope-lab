803de5b81ca69d657642a5238f96448404003976
Natasha Bartley <+>
2023-03-25 22:40:47 -0400

    passing lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'someone else';
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'jessie';
+}
+
